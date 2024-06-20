518c9aa7c77222c41d851b34c44125c09c63cda1
Carol Tagliaferro <caroltagliaferro1@gmail.com>
2024-06-20 00:24:03 -0300

    style: tags imagens carrossel

--- loja-moveis/src/pages/Home/Components/carrousel.tsx
+++ loja-moveis/src/pages/Home/Components/carrousel.tsx
@@ -1,6 +1,7 @@
 import { Splide, SplideSlide } from "react-splide-ts";
 import "react-splide-ts/css";
-import "../../../App.css";
+import "../Components/carrosel.css";
+import { GoArrowRight } from "react-icons/go";
 
 const Carrousel = () => {
   return (
@@ -23,23 +24,49 @@ const Carrousel = () => {
       className="splide-custom"
     >
       <SplideSlide>
-        <img
-          src="https://furniro-at.s3.amazonaws.com/Carousel/Carousel1.png"
-          alt="Carousel 1"
-          className="zoom-effect"
-        />
+        <div className="relative">
+          <img
+            src="https://furniro-at.s3.amazonaws.com/Carousel/Carousel1.png"
+            alt="Carousel 1"
+          />
+          <div className="absolute w-48 left-5 top-3/4 transform -translate-y-1/2 text-center py-5 bg-white bg-opacity-80">
+            <p className="text-sm text-grayTwo">01 - Bed Room</p>
+            <p className="text-2xl font-semibold text-font">Inner Peace</p>
+          </div>
+          <button className="absolute top-3/4 right-48 transform  bg-primary w-10 h-10 p-1">
+            <GoArrowRight className="text-white w-6 h-6" />
+          </button>
+        </div>
       </SplideSlide>
       <SplideSlide>
-        <img
-          src="https://furniro-at.s3.amazonaws.com/Carousel/Carousel2.png"
-          alt="Carousel 2"
-        />
+        <div className="relative">
+          <img
+            src="https://furniro-at.s3.amazonaws.com/Carousel/Carousel2.png"
+            alt="Carousel 2"
+          />
+          <div className="absolute w-48 left-5 top-3/4  text-center py-5 bg-white bg-opacity-80">
+            <p className="text-sm text-grayTwo">02 - Living Room</p>
+            <p className="text-2xl font-semibold text-font">Cozy Space</p>
+          </div>
+          <button className="absolute top-3/4 right-48  bg-primary w-10 h-10 p-1">
+            <GoArrowRight className="text-white w-6 h-6" />
+          </button>
+        </div>
       </SplideSlide>
       <SplideSlide>
-        <img
-          src="https://furniro-at.s3.amazonaws.com/Carousel/Carousel3.png"
-          alt="Carousel 3"
-        />
+        <div className="relative">
+          <img
+            src="https://furniro-at.s3.amazonaws.com/Carousel/Carousel3.png"
+            alt="Carousel 3"
+          />
+          <div className="absolute w-48 left-5 top-3/4  text-center py-5 bg-white bg-opacity-80">
+            <p className="text-sm text-grayTwo">03 - Dining Room</p>
+            <p className="text-2xl font-semibold text-font">Elegant Room</p>
+          </div>
+          <button className="absolute top-96 right-48  bg-primary w-10 h-10 p-1">
+            <GoArrowRight className="text-white w-6 h-6" />
+          </button>
+        </div>
       </SplideSlide>
     </Splide>
   );
--- loja-moveis/src/pages/Home/Components/fourthSection.tsx
+++ loja-moveis/src/pages/Home/Components/fourthSection.tsx
@@ -3,23 +3,23 @@ import "../../../App.css";
 
 const FourthSection = () => {
   return (
-    <div className="bg-lightBg font-poppins flex h-full py-8 lg:py-10 lg:pl-20">
-      <div className="lg:ml-10 ml-4 lg:p-5 pr-0 flex justify-between items-center">
+    <div className="bg-lightBg font-poppins flex h-full py-8 lg:py-10 lg:pl-20 relative">
+      <div className="lg:ml-10 ml-4 lg:p-5 pr-0 flex justify-between items-center w-full">
         <div className="flex flex-col justify-center w-1/3 mr-10">
           <h1 className="font-bold text-xl lg:text-5xl text-font mb-2">
             50+ Beautiful rooms
             <br /> inspiration
           </h1>
-          <p className="font-medium text-xs lg:text-base  text-gray-600 mb-3 lg:mb-6">
+          <p className="font-medium text-xs lg:text-base text-gray-600 mb-3 lg:mb-6">
             Our designer already made a lot of beautiful
             <br />
-            prototipe of rooms that inspire you
+            prototype of rooms that inspire you
           </p>
           <button className="bg-primary hover:bg-primaryDark text-white lg:text-md text-sm lg:font-medium py-1 lg:py-3 lg:px-8 w-26 lg:w-1/3">
             Explore More
           </button>
         </div>
-        <div className="flex-1">
+        <div className="flex-1 relative">
           <Carrousel />
         </div>
       </div>
