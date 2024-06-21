aca08f0f20809ddcb5442c53ef6231382b850427
Carol Tagliaferro <caroltagliaferro1@gmail.com>
2024-06-21 13:43:03 -0300

    fix: disposição das imagens ultima seção da home

--- loja-moveis/src/pages/Home/Components/fifthSection.tsx
+++ loja-moveis/src/pages/Home/Components/fifthSection.tsx
@@ -1,61 +1,69 @@
-import React from "react";
-
 const FifthSection = () => {
   return (
     <div className="my-5">
       <div className="font-poppins text-center">
-        <p className="font-semibold text-gray-600 ">Share your setup with</p>
+        <p className="font-semibold text-gray-600">Share your setup with</p>
         <h1 className="text-4xl font-bold text-font">#FurniroFurniture</h1>
       </div>
-      <div className="">
-        <img
-          className=""
-          src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+36.png"
-          alt=""
-        />
-        <img
-          className=""
-          src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+38.png"
-          alt=""
-        />
-
-        <img
-          className=""
-          src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+37.png"
-          alt=""
-        />
-        <img
-          className=""
-          src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+39.png"
-          alt=""
-        />
-      </div>
-
       <div className="flex justify-center">
-        <div className="">
+        <div className="flex flex-col">
+          <div className="flex mb-4">
+            <img
+              className="mr-2 "
+              src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectanglee+36.png"
+              alt=""
+            />
+            <img
+              className="ml-2"
+              src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+38.png"
+              alt=""
+            />
+          </div>
+          <div className="flex">
+            <img
+              className="mr-2"
+              src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+37.png"
+              alt=""
+            />
+            <img
+              className="ml-2"
+              src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+39.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="mx-4 flex items-center">
           <img
             src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+40.png"
             alt=""
           />
         </div>
-      </div>
-      <div className="grid grid-rows-2 grid-flow-col ml-10">
-        <img
-          src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+43.png"
-          alt=""
-        />
-        <img
-          src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+45.png"
-          alt=""
-        />
-        <img
-          src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+41.png"
-          alt=""
-        />
-        <img
-          src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+44.png"
-          alt=""
-        />
+        <div className="flex flex-col">
+          <div className="flex mb-4">
+            <img
+              className="mr-2"
+              src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+43.png"
+              alt=""
+            />
+            <img
+              className="ml-2"
+              src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+45.png"
+              alt=""
+            />
+          </div>
+          <div className="flex">
+            <img
+              className="mr-2"
+              src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+41.png"
+              alt=""
+            />
+            <img
+              className="ml-2"
+              src="https://furniro-at.s3.amazonaws.com/HomeImages/Rectangle+44.png"
+              alt=""
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
