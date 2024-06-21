51afee6822b0cc05b9bc34216d3efbde459dae5f
Carol Tagliaferro <caroltagliaferro1@gmail.com>
2024-06-21 03:21:35 -0300

    Style: página de erro estilizada

--- loja-moveis/src/components/errorPage.tsx
+++ loja-moveis/src/components/errorPage.tsx
@@ -1,7 +1,38 @@
+import { NavLink } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
-    <div>
-      <div className="text-black text-2xl text-center">Error 404</div>
+    <div className=" flex justify-center items-center">
+      <div className="relative font-poppins">
+        <img
+          src="https://furniro-at.s3.amazonaws.com/bgError.jpg"
+          alt="Abandoned Sofa"
+          className="grayscale brightness-50 lg:w-full lg:h-full h-80 w-auto"
+        />
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+          <p className="lg:text-2xl text-sm font-semibold text-primary">
+            ERROR
+          </p>
+          <h1 className="lg:text-9xl text-5xl font-extrabold text-primary">
+            404
+          </h1>
+          <div className="my-3 text-white">
+            <p className="lg:text-3xl text-sm font-medium">
+              Oh no! Someone abandoned this...
+            </p>
+            <p className="lg:text-lg text-xs font-light">
+              The page you're looking for doesn't exist.
+            </p>
+          </div>
+          <div>
+            <NavLink to={"/"}>
+              <button className="bg-primary hover:bg-primaryDark lg:text-lg text-sm px-2 py-1 lg:px-5 lg:py-2 rounded-lg text-white">
+                Go back home
+              </button>
+            </NavLink>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
